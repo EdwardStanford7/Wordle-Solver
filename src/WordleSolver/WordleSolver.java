381db00519901faac4ecc11220cf2963a742445b
Edward Stanford <edwardstanford7@gmail.com>
2022-01-20 16:00:11 -0700

    Deleted unused class

--- src/WordleSolver/WordleSolver.java
+++ src/WordleSolver/WordleSolver.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class WordleSolver 
 {
 	public static void main(String args[]) throws FileNotFoundException
-	{	
+	{
 		RankedWords rankedWords = new RankedWords();
 		
 		boolean correctAnswer = false;
