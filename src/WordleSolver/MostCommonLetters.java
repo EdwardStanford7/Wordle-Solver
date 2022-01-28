7172c7176f24d96d1da354e4bacdc12b9220817a
Edward Stanford <edwardstanford7@gmail.com>
2022-01-28 14:55:07 -0700

    Rename

--- src/WordleSolver/MostCommonLetters.java
+++ src/WordleSolver/MostCommonLetters.java
@@ -16,11 +16,15 @@ public class MostCommonLetters
 		int[][] numCharUses = new int[5][26];
 		
 		Scanner s = new Scanner(new File("src/wordle_words.txt"));
+		//Scanner s = new Scanner(new File("src/all_five_letter_words.txt")); // For hellowordl
 		
 		while (s.hasNext())
 		{
 			String word = s.next();
-			wordleWords.add(word);
+			if(! wordleWords.contains(word))
+			{
+				wordleWords.add(word);
+			}
 			
 			for(int i = 0; i < 5; ++i)
 			{
